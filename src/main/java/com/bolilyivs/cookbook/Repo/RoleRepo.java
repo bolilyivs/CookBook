d7aaaaf76a6721ddeb588f0f84115ce69381a458
Vladislav <bolilyivs@gmail.com>
2019-07-15 17:53:27 +0500

    Clear

--- src/main/java/com/bolilyivs/cookbook/Repo/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bolilyivs.cookbook.Repo;
-
-import com.bolilyivs.cookbook.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<com.bolilyivs.cookbook.Entity.Role, Long> {
-    com.bolilyivs.cookbook.Entity.Role findByName(String name);
-}
